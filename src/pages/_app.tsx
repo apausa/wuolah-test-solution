d7202b5981aeaa6cc4e673bd6123e5a81503a489
Ivan Coronado Moreno <ivacormor@gmail.com>
2022-06-06 16:51:09 +0200

    chore(react-query): install and config allowing hydrating the cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import type { AppProps } from "next/app";
+
+import { QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { client } from "api/client";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => client);
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
