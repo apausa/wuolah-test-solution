caacdca8dbac9ca18d3ee2730decab0d25c8d195
Ivan Coronado Moreno <ivacormor@gmail.com>
2022-06-06 18:20:52 +0200

    feat(universidades): add virtualized infinite scroll

--- src/pages/universidades/index.tsx
+++ src/pages/universidades/index.tsx
@@ -1,14 +1,16 @@
 import { useMemo } from "react";
 import NextLink from "next/link";
+import { Virtuoso } from "react-virtuoso";
 
 import { useInfiniteUniversities } from "api/hooks/useUniversities";
 
-import { Heading, Stack, Link } from "@chakra-ui/react";
+import { Heading, Link, Spinner, Text } from "@chakra-ui/react";
 
 const Page = () => {
-  const { data } = useInfiniteUniversities({});
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
+    useInfiniteUniversities({});
 
-  const flatUniversities = useMemo(
+  const items = useMemo(
     () =>
       data
         ? data.pages.flatMap(({ data }) => data.map((university) => university))
@@ -17,19 +19,32 @@ const Page = () => {
   );
 
   return (
-    <div>
+    <>
       <Heading as="h1">Universidades</Heading>
-      <Stack>
-        {flatUniversities.map((university) => (
-          <NextLink
-            key={university.id}
-            href={`/universidades/${university.slug}`}
-          >
-            <Link>{university.name}</Link>
-          </NextLink>
-        ))}
-      </Stack>
-    </div>
+      <Virtuoso
+        data={items}
+        useWindowScroll
+        endReached={() => fetchNextPage()}
+        overscan={20}
+        itemContent={(index, university) => {
+          return (
+            <NextLink
+              key={university.id}
+              href={`/universidades/${university.slug}`}
+            >
+              <Link>{university.name}</Link>
+            </NextLink>
+          );
+        }}
+        components={{
+          Footer: () => {
+            if (!hasNextPage) return <Text>No hay más resultados</Text>;
+            if (isFetchingNextPage) return <Spinner />;
+            return null;
+          },
+        }}
+      />
+    </>
   );
 };
 
